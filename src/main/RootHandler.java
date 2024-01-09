07a18583b0a7566f77384f24496efb7bdfb864a2
Naureen <naureen.imran123@yahoo.com>
2024-01-09 12:31:15 +0000

    refactor

--- src/main/RootHandler.java
+++ src/main/RootHandler.java
@@ -55,45 +55,45 @@ public class RootHandler implements HttpHandler {
                         "<h1 class=\"text-center\"> The Food Store !</h1>");
         if (Objects.equals(loggedInUser, "admin")) {
 
-                       out.write( "    <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">" +
-                        "      <div class=\"container-fluid\">" +
-                        "        <a class=\"navbar-brand\" href=\"/\">Food Store</a>" +
-                        "        <button class=\"navbar-toggler\" type=\"button\" " +
-                        "data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
-                        "          <span class=\"navbar-toggler-icon\"></span>" +
-                        "        </button>" +
-                        "        <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">" +
-                        "          <ul class=\"navbar-nav\">" +
-                        "            <li class=\"nav-item\">" +
-                        "              <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>" +
-                        "            </li>" +
-                        "            <li class=\"nav-item\">" +
-                        "              <a class=\"nav-link\" href=\"/customers\">Customers</a>" +
-                        "            </li>" +
-                        "            <li class=\"nav-item\">" +
-                        "              <a class=\"nav-link\" href=\"/stock\">Stock</a>" +
-                        "            </li>" +
-                        "            </li>" +
-                        "          </ul>" +
-                        "        </div>" +
-                        "<nav class=\"navbar bg-body-tertiary navbar-dark bg-dark \">" +
-                        "      <div class=\"container-fluid \">" +
-                        "<form class=\"d-flex\" method=\"GET\" action=\"/search\">" +
-                        "          <input" +
-                        "            class=\"form-control me-2\"" +
-                        "            type=\"search\"" +
-                        "            name=\"query\"" +
-                        "            placeholder=\"Search Products\"" +
-                        "            aria-label=\"Search\"" +
-                        "          />" +
-                        "          <button class=\"btn btn-outline-success\" type=\"submit\">Search</button>" +
-                        "        </form>" +
-                        "      </div>" +
-                        "    </nav>" +
-                        "      </div>" +
-                        "    </nav>" );
-        }else {
-            out.write( "    <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">" +
+            out.write("    <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">" +
+                    "      <div class=\"container-fluid\">" +
+                    "        <a class=\"navbar-brand\" href=\"/\">Food Store</a>" +
+                    "        <button class=\"navbar-toggler\" type=\"button\" " +
+                    "data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNavDropdown\" aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">" +
+                    "          <span class=\"navbar-toggler-icon\"></span>" +
+                    "        </button>" +
+                    "        <div class=\"collapse navbar-collapse\" id=\"navbarNavDropdown\">" +
+                    "          <ul class=\"navbar-nav\">" +
+                    "            <li class=\"nav-item\">" +
+                    "              <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>" +
+                    "            </li>" +
+                    "            <li class=\"nav-item\">" +
+                    "              <a class=\"nav-link\" href=\"/customers\">Customers</a>" +
+                    "            </li>" +
+                    "            <li class=\"nav-item\">" +
+                    "              <a class=\"nav-link\" href=\"/stock\">Stock</a>" +
+                    "            </li>" +
+                    "            </li>" +
+                    "          </ul>" +
+                    "        </div>" +
+                    "<nav class=\"navbar bg-body-tertiary navbar-dark bg-dark \">" +
+                    "      <div class=\"container-fluid \">" +
+                    "<form class=\"d-flex\" method=\"GET\" action=\"/search\">" +
+                    "          <input" +
+                    "            class=\"form-control me-2\"" +
+                    "            type=\"search\"" +
+                    "            name=\"query\"" +
+                    "            placeholder=\"Search Products\"" +
+                    "            aria-label=\"Search\"" +
+                    "          />" +
+                    "          <button class=\"btn btn-outline-success\" type=\"submit\">Search</button>" +
+                    "        </form>" +
+                    "      </div>" +
+                    "    </nav>" +
+                    "      </div>" +
+                    "    </nav>");
+        } else {
+            out.write("    <nav class=\"navbar navbar-expand-lg navbar-dark bg-dark\">" +
                     "      <div class=\"container-fluid\">" +
                     "        <a class=\"navbar-brand\" href=\"/\">Food Store</a>" +
                     "        <button class=\"navbar-toggler\" type=\"button\" " +
@@ -122,25 +122,25 @@ public class RootHandler implements HttpHandler {
                     "      </div>" +
                     "    </nav>" +
                     "      </div>" +
-                    "    </nav>" );
+                    "    </nav>");
         }
 
-                out.write(        "<div class=\"hstack gap-3\">" +
-                        "<div class=\"dropdown\">" +
-                        "      <button class=\"btn btn-success dropdown-toggle p-3 mt-2\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">" +
-                        "        Choose Category" +
-                        "      </button>" +
-                        "      <ul class=\"dropdown-menu\" style=\"\">" +
-                        "        <li><a class=\"dropdown-item\" href=\"filter?Category=Fruit\">Fruit</a></li>" +
-                        "        <li><a class=\"dropdown-item\" href=\"filter?Category=Vegetable\">Vegetable</a></li>" +
-                        "        <li><a class=\"dropdown-item\" href=\"filter?Category=Cold Drink\">Cold Drink</a></li>" +
-                        "        <li><a class=\"dropdown-item\" href=\"filter?Category=Frozen Food\">Frozen Food</a></li>" +
-                        "        <li><a class=\"dropdown-item\" href=\"filter?Category=Snack\">Snack</a></li>" +
-                        "<li><a class=\"dropdown-item\" href=\"/\">All</a></li>" +
-                        "      </ul>" +
-                        "    </div>" +
+        out.write("<div class=\"hstack gap-3\">" +
+                "<div class=\"dropdown\">" +
+                "      <button class=\"btn btn-success dropdown-toggle p-3 mt-2\" type=\"button\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\">" +
+                "        Choose Category" +
+                "      </button>" +
+                "      <ul class=\"dropdown-menu\" style=\"\">" +
+                "        <li><a class=\"dropdown-item\" href=\"filter?Category=Fruit\">Fruit</a></li>" +
+                "        <li><a class=\"dropdown-item\" href=\"filter?Category=Vegetable\">Vegetable</a></li>" +
+                "        <li><a class=\"dropdown-item\" href=\"filter?Category=Cold Drink\">Cold Drink</a></li>" +
+                "        <li><a class=\"dropdown-item\" href=\"filter?Category=Frozen Food\">Frozen Food</a></li>" +
+                "        <li><a class=\"dropdown-item\" href=\"filter?Category=Snack\">Snack</a></li>" +
+                "<li><a class=\"dropdown-item\" href=\"/\">All</a></li>" +
+                "      </ul>" +
+                "    </div>" +
 
-                        "  <div class=\"p-2 ms-auto\">");
+                "  <div class=\"p-2 ms-auto\">");
         if (loggedInUser != null) {
             out.write("<a href=\"/logout\" class=\"btn btn-dark p-3 mt-2\"> Log Out</a>");
         } else {
@@ -150,15 +150,16 @@ public class RootHandler implements HttpHandler {
         }
         out.write("</div>" +
                 " <div class=\"vr\"></div>" +
-                "<div class=\"p-2\">" );
+                "<div class=\"p-2\">");
         if (!Objects.equals(loggedInUser, "admin")) {
 
-               out.write( "<a href=\"/viewbasket\" class=\"btn btn-dark mt-2 p-3\"> " +
-                "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-basket-fill\" viewBox=\"0 0 16 16\">" +
-                "  <path d=\"M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z\"/>" +
-                "</svg>" +
-                "   Shopping Basket </a>" );}
-               out.write( "</div>" +
+            out.write("<a href=\"/viewbasket\" class=\"btn btn-dark mt-2 p-3\"> " +
+                    "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" fill=\"currentColor\" class=\"bi bi-basket-fill\" viewBox=\"0 0 16 16\">" +
+                    "  <path d=\"M5.071 1.243a.5.5 0 0 1 .858.514L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v5a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V9H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 6h1.717zM3.5 10.5a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0zm2.5 0a.5.5 0 1 0-1 0v3a.5.5 0 0 0 1 0z\"/>" +
+                    "</svg>" +
+                    "   Shopping Basket </a>");
+        }
+        out.write("</div>" +
                 "    </div >" +
                 "<br>" +
                 "<br>" +
